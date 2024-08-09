7865ccaf9a7c3c883cf9806da5d47cd93cc079b7
Dadayev <dadayevkerim@gmail.com>
2024-08-09 11:54:08 +0500

    Add image placeholder in Card

--- vite-project/src/components/Card/Card.tsx
+++ vite-project/src/components/Card/Card.tsx
@@ -1,11 +1,34 @@
-import React, { FC } from "react";
+import React, { FC, useCallback, useState } from "react";
 import styles from "./Card.module.css";
 import { CardProps } from "../../types/types";
 import defaultImg from "../../assets/react.svg";
+import placeholder from "../../assets/placeholder.jpg";
+
 const Card: FC<CardProps> = (props: CardProps) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  };
+
+  const handleImageError = () => {
+    setIsLoaded(false);
+  };
   return (
     <div className={styles.card}>
-      <img src={props.image || defaultImg} alt="" />
+      {!isLoaded && (
+        <img
+          src={placeholder}
+          alt="placeholder"
+        />
+      )}
+      <img
+        src={props.image || defaultImg}
+        alt="picture"
+        onLoad={handleImageLoad}
+        onError={handleImageError}
+        style={{ display: isLoaded ? "block" : "none" }}
+      />
       <h2>{props.title}</h2>
       <p>{props.date_display}</p>
       <p>{props.artist_display}</p>
